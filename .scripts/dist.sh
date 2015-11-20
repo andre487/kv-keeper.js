e5b71e20d32e823e0377b8e4af21ad650f48974e
andre487 <andre487@yandex-team.ru>
2015-11-21 01:07:51 +0300

    Fix dist banner

--- .scripts/dist.sh
+++ .scripts/dist.sh
@@ -9,7 +9,7 @@ sed "s/'use strict';//" lib/kv-keeper.js > dist/kv-keeper.work.js
 uglifyjs dist/kv-keeper.work.js --output dist/kv-keeper.min.js \
     --source-map=dist/kv-keeper.map \
     --source-map-url=kv-keeper.map \
-    --preamble="// Key-Value Keeper by andre487, see https://clck.ru/9cB92" \
+    --preamble="// KV-Keeper.js by andre487, see https://clck.ru/9cB92" \
     --mangle="sort=true" \
     --compress \
     --screw-ie8 \
