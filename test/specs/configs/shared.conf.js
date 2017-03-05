3edbeef763993889145843803a949e80db6e812a
andre487 <andre487@yandex-team.ru>
2017-03-06 02:53:33 +0300

    Remove host option

--- test/specs/configs/shared.conf.js
+++ test/specs/configs/shared.conf.js
@@ -26,8 +26,6 @@ module.exports = function (config) {
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         reporters: ['mocha'],
 
-        listenAddress: 'localhost',
-
         // web server port
         port: 9876,
 
