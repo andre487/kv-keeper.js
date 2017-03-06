91138addfa93b5362406f959b3381e0c1ea06996
andre487 <andre487@yandex-team.ru>
2017-03-06 03:16:45 +0300

    Add Chrome, Firefox and Safari testing

---
+++ test/specs/configs/safari-lib.conf.js
@@ -0,0 +1,9 @@
+module.exports = function (config) {
+    require('./shared.conf.js')(config);
+
+    config.set({
+        files: ['../../../lib/kv-keeper.js'].concat(config.files),
+
+        browsers: ['Safari']
+    });
+};
